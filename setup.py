3b0168e61f067195ef9bf4704b37fb434310f24c
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-09 09:49:14 +0530

    Added the setup script

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+DEPENDENCIES = []
+
+
+setuptools.setup(
+    name="python-polyglot",  # Replace with your own username
+    version="1.0.0",
+    author="P Pranav Baburaj",
+    author_email="code-roller@googlegroups.com",
+    description="Find the percentage of programming languages used in your project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pranavbaburaj/polyglot",
+    packages=setuptools.find_packages(),
+    install_requires=DEPENDENCIES,
+    entry_points={"console_scripts": [
+        
+        ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
