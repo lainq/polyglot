556f80d23455738a0c998e46aba3f83b72c3bce4
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-09 10:54:56 +0530

    Created the basic file downloader

--- polyglot/polyglot.py
+++ polyglot/polyglot.py
@@ -1,6 +1,11 @@
 from .exceptions import *
 import os as os
 import logging as Logger
+import requests as Requests
+import yaml as YamlLoader
+import pprint as pprint
+
+printer = pprint.PrettyPrinter()
 
 class PolyglotExtensions(object):
     def __init__(self, param):
@@ -21,23 +26,47 @@ class PolyglotExtensions(object):
             else:
                 return os.path.join(os.getcwd(), param)
 
-    def find_files(self):
-        langauge_file = os.path.exists("../language.yaml")
-        if not langauge_file:
+    def find_files(self, yaml_file):
+        if yaml_file == None:
             Logger.error("Cannot find the language file")
-            self.__install_language_file()
+            yaml_file = self.__install_language_file()
+        with open(yaml_file, "r") as yaml_reader:
+            file_content = YamlLoader.load(
+                yaml_reader,
+                Loader=YamlLoader.FullLoader
+            )
+        
+        for language in dict(file_content):
+            pass
 
     def __install_language_file(self):
-        pass
+        filename = os.path.join(os.getcwd(), "language.yml")
+        url = "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
+        Logger.info(f"Installing file from {url} to {filename}")
+        try:
+            open(
+                filename,
+                "wb"
+            ).write(
+                Requests.get(url, allow_redirects=True).content
+            )
+        except Exception as exception:
+            Logger.info("Process failed")
+            raise exception
+
+        return filename
+
 
 
 class Polyglot(object):
-    def __init__(self, command="init", command_argument="."):
+    def __init__(self, command="init", command_argument=".", language_yml=None):
         assert isinstance(command, str), "Expected a string"
         assert isinstance(command_argument, str), "Expected a strng"
 
         self.command = command
         self.parameters = command_argument
+
+        self.language_yaml = language_yml
 
         self.files = {}
 
@@ -47,4 +76,4 @@ class Polyglot(object):
         the reuslts as an array(list)
         """
         if self.command == "init":
-            poly = PolyglotExtensions(self.parameters).find_files()
+            poly = PolyglotExtensions(self.parameters).find_files(self.language_yaml)
