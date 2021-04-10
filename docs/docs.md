f934cfa184c29427e95c0a035d6dcf73b27c493d
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-10 18:54:04 +0530

    Set up docs and tests

---
+++ docs/docs.md
@@ -0,0 +1,3 @@
+# Docs
+
+Under construction
---
+++ tests/test_polyglot.py
@@ -0,0 +1,14 @@
+from context import polyglot
+
+import unittest
+
+
+class TestCases(unittest.TestCase):
+    """Advanced test cases."""
+
+    def test_application(self):
+        self.assertIsNone(None)
+
+
+if __name__ == '__main__':
+    unittest.main()
