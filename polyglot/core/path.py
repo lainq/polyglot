9c718f19abd3dca4e17ec54bd8ad4392edcb91d6
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-17 12:41:09 +0530

    Initialized Path

---
+++ polyglot/core/path.py
@@ -0,0 +1,10 @@
+import os
+
+class Path(object):
+    def __init__(self, path=None, env=None):
+        self.path = self.path
+        self.env = None
+
+    @property
+    def language_file_path()
+        
