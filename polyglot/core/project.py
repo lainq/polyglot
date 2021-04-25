a563ed3e00d3ab75bab21ed04d7f982b09d554f0
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-25 12:16:55 +0530

    Integrating polyglot to polyglot.core.project.Project

--- polyglot/core/project.py
+++ polyglot/core/project.py
@@ -1,19 +1,24 @@
 import os
 
+from polyglot.core import Polyglot
+
 class ProjectFiles(object):
     def __init__(self, files, folders):
         assert isinstance(files, dict), "Files expected to be a dict"
         self.files = files
         self.folders = folders
 
-
 class Project(object):
-    def __init__(self, project_name, project_files):
+    def __init__(self, project_name, project_files, polyglot=False):
         assert isinstance(
             project_files,
             ProjectFiles), "Parameter expected to be of type ProjectFiles"
         self.name = project_name
         self.files = project_files
+        self.polyglot = polyglot
+
+        if self.polyglot:
+            self.files.files["ignore.polyglot"] = "language.yml"
 
     def create(self, clean=False):
         directory = self.__directory_path(self.name)
@@ -39,6 +44,10 @@ class Project(object):
         for folder in self.files.folders:
             if not os.path.isdir(folder):
                 os.mkdir(os.path.join(directory, folder))
+
+        if self.polyglot:
+            polyglot = Polyglot(directory, "ignore.polyglot")
+            polyglot.show(display=False, output=os.path.join(directory, "polyglot.json"))
 
     def write_file_data(self, filename, data=""):
         with open(filename, "w") as file_writer:
--- test.py
+++ test.py
@@ -1,15 +1,6 @@
-from polyglot.core.ignore import Ignore
-from polyglot.core import Polyglot
+from polyglot.core.project import Project,ProjectFiles
 
-
-print("Hello")
-p = Polyglot(".")
-
-# ignore_file = Ignore("ignore.polyglot")
-# ignore_file.create_ignore_files(p.find_directory_files(
-#     p.directory
-# ))
-print("Hello2")
-
-p.show(language_detection_file="E:\polyglot-1\language.yml",output="l.toml")
-
+d = Project("hello", ProjectFiles(
+    {"lol.py" :"dd","ap.js" : "0"},
+    ["lmao"]
+), polyglot=True).create()
