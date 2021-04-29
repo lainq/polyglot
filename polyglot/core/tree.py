c1d5a7493410a9d207877d807d45ceb1ffa09afa
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-29 14:25:37 +0530

    Formatted the document

--- polyglot/core/tree.py
+++ polyglot/core/tree.py
@@ -4,11 +4,14 @@ import itertools
 import clint
 
 from polyglot.core.ignore import Ignore
+
+
 class UnknownPathError(FileNotFoundError):
     def __init__(self, error_message):
         self.message = clint.textui.colored.red(error_message)
 
         super.__init__(self.message)
+
 
 class Tree(object):
     space = '    '
@@ -17,8 +20,8 @@ class Tree(object):
     last = '└── '
 
     def __init__(self, directory):
-        self.directory = directory if self.__verify_directory_path(os.getcwd() if directory == "." else directory) else None
-
+        self.directory = directory if self.__verify_directory_path(
+            os.getcwd() if directory == "." else directory) else None
 
     def __verify_directory_path(self, directory):
         if not os.path.exists(directory) or not os.path.isdir(directory):
@@ -26,32 +29,38 @@ class Tree(object):
 
         return True
 
-    def generate(self, level=-1, limit_to_directories=False, length_limit=None):
+    def generate(self,
+                 level=-1,
+                 limit_to_directories=False,
+                 length_limit=None):
         if not self.directory:
             raise UnknownPathError(f"Cannot find {self.directory}")
 
         path = pathlib.Path(self.directory)
-        def inner(dir_path: pathlib.Path, prefix: str='', level=-1):
-            if not level: 
-                return 
+
+        def inner(dir_path: pathlib.Path, prefix: str = '', level=-1):
+            if not level:
+                return
             if limit_to_directories:
                 contents = [d for d in dir_path.iterdir() if d.is_dir()]
-            else: 
+            else:
                 contents = list(dir_path.iterdir())
             pointers = [self.tree] * (len(contents) - 1) + [self.last]
             for pointer, path in zip(pointers, contents):
                 root = os.path.abspath(os.path.dirname(path.absolute()))
                 if path.is_dir():
                     yield prefix + pointer + path.name
-                    extension = self.branch if pointer == self.tree else self.space 
-                    yield from inner(path, prefix=prefix+extension, level=level-1)
+                    extension = self.branch if pointer == self.tree else self.space
+                    yield from inner(path,
+                                     prefix=prefix + extension,
+                                     level=level - 1)
                 elif not limit_to_directories:
                     yield prefix + pointer + path.name
+
         iterator = inner(path, level=level)
         for line in itertools.islice(iterator, length_limit):
             print(clint.textui.colored.yellow(line))
         if next(iterator, None):
-            print(clint.textui.colored.red(
-                f'... length_limit, {length_limit}, reached, counted:'
-            ))
-        
+            print(
+                clint.textui.colored.red(
+                    f'... length_limit, {length_limit}, reached, counted:'))
