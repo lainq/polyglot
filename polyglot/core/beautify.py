c4a4da77d57a6632809af2df06c5029adbf37179
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-04 13:02:29 +0530

    Formatted using yapf

--- polyglot/core/beautify.py
+++ polyglot/core/beautify.py
@@ -2,6 +2,20 @@ import os
 
 from polyglot.path import DirectoryError
 
+
+class _Prompt(object):
+    def __init__(self, prompt, options, require=True):
+        self.prompt = prompt
+        self.options = options
+        self.require = require
+
+    def create_prompt(self):
+        data = None
+        while data not in self.options:
+            data = input(f"{self.prompt} (y/n) ")
+        return data == "y"
+
+
 class Beautify(object):
     def __init__(self, directory, extensions, prompt=True):
         self.directory = self.__is_directory(directory)
@@ -10,6 +24,16 @@ class Beautify(object):
         self.clean_directory(prompt)
 
     def clean_directory(self, prompt):
+        if prompt:
+            prompt = _Prompt("Do you want to clear the directory",
+                             ["y", "n"]).create_prompt()
+            if prompt:
+                self.__clean()
+                return None
+
+        self.__clean()
+
+    def __clean(self):
         pass
 
     def __is_directory(self, directory):
