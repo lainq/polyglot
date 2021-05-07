7a40d326153ca588203e7a819f5596f5a9b5963f
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-07 20:29:21 +0530

    ...

--- polyglot/ext/env.py
+++ polyglot/ext/env.py
@@ -2,11 +2,13 @@ import os
 
 from clint.textui import colored
 
+
 def find_token_type(value):
     if value.isdigit():
         return (int, float)
-    
+
     return (str)
+
 
 class EnvAssignmentError(Exception):
     def __init__(self, error, line_number):
@@ -16,12 +18,15 @@ class EnvAssignmentError(Exception):
         error_message = colored.red(f"{self.message} at line {self.line}")
         super().__init__(error_message)
 
+
 class InvalidVariableName(EnvAssignmentError):
     def __init__(self, message, line):
         super().__init__(message, line)
 
+
 class Tokens(object):
     COMMENT_TOKEN = "#"
+
 
 class EnvironmentVariable(object):
     def __init__(self, variable, value, token_type, line_number):
@@ -33,10 +38,12 @@ class EnvironmentVariable(object):
 
     def __check_variable_name(self, name):
         if len(name) == 0:
-            raise InvalidVariableName(f"Invalid variable name {name}", self.line)
+            raise InvalidVariableName(f"Invalid variable name {name}",
+                                      self.line)
         first = name[0]
         if first.isdigit():
-            raise InvalidVariableName(f"Variable name {name} starts with a number", self.line)
+            raise InvalidVariableName(
+                f"Variable name {name} starts with a number", self.line)
 
         return name
 
@@ -57,6 +64,7 @@ class EnvParserPosition(object):
 
         return data[self.position]
 
+
 class EnvParser(object):
     tokens = []
 
@@ -67,22 +75,23 @@ class EnvParser(object):
         self.character = self.position.current_character(self.source)
 
     def create_parser_tokens(self):
-        if len(self.source) == 0 or self.source.startswith(Tokens.COMMENT_TOKEN):
+        if len(self.source) == 0 or self.source.startswith(
+                Tokens.COMMENT_TOKEN):
             return []
-
 
         assignment_counts = self.source.count("=")
         if assignment_counts > 1 or assignment_counts == 0:
-            raise EnvAssignmentError("Multiple or no assignments ", self.line_number)
-        
+            raise EnvAssignmentError("Multiple or no assignments ",
+                                     self.line_number)
+
         name, value = self.source.split("=")
         token_type = find_token_type(value)
-        existing_variables = list(filter(
-            lambda list_element : list_element.variable == name,
-            self.tokens
-        ))
+        existing_variables = list(
+            filter(lambda list_element: list_element.variable == name,
+                   self.tokens))
         if not len(existing_variables) == 0:
-            raise InvalidVariableName(f"Duplicate variable name {name}", self.line_number)
+            raise InvalidVariableName(f"Duplicate variable name {name}",
+                                      self.line_number)
         token = EnvironmentVariable(name, value, token_type, self.line_number)
         self.tokens.append(token)
 
@@ -92,11 +101,13 @@ class EnvParser(object):
         self.position.increment(1)
         self.character = self.position.current_character(self.source)
 
+
 class Env(object):
     defualt_filename = os.path.join(os.getcwd(), ".env")
 
     def __init__(self, env=None, load=True):
-        assert isinstance(env, str) or env == None, "Unexpected type of parameter env"
+        assert isinstance(
+            env, str) or env == None, "Unexpected type of parameter env"
         self.env = env or self.defualt_filename
         self.load_to_process = load
 
@@ -108,10 +119,9 @@ class Env(object):
             token_data = parser.create_parser_tokens()
             for token_element in token_data:
                 tokens.append(token_element)
-        
+
         for token_element in tokens:
             os.environ.setdefault(token_element.variable, token_element.value)
-        
 
     def __read(self, filename):
         if not os.path.exists(filename) and os.path.isfile(filename):
