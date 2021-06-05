2acd6e120fe1c506e852360fc8f4f23fe06fa1e9
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-06-05 15:56:51 +0530

    feat(cli):ommand execution

--- polyglot/__main__.py
+++ polyglot/__main__.py
@@ -1,6 +1,14 @@
 import sys
+import os
 
 from clint.textui import colored
+
+COMMANDS = [
+    "stats"
+]
+
+class EventLogger(object):
+    pass
 
 class CommandLineException(object):
     def __init__(self, message, suggestion=None, fatal=True):
@@ -41,7 +49,7 @@ class ArgumentParser(object):
                 )
 
             statement = current.split("=")
-            parameter_key, value = statement[0], ""
+            parameter_key, value = statement[0], -2
             if len(statement) > 1:
                 value = "=".join(map(str, statement[1:]))
             parameters.setdefault(parameter_key, value)
@@ -50,11 +58,37 @@ class ArgumentParser(object):
     def create_help_command(self):
         print("Help")
 
+class LanguageStats(object):
+    def __init__(self, parameters):
+        self.directory = parameters.get("--dir") or os.getcwd()
+        self.ignore = parameters.get("--ignore") or -1
+        
+        if self.ignore == -2:
+           self.ignore = self.__find_ignore_file()
+        else:
+            self.ignore = None
+        
+        print(self.ignore)
+
+    def __find_ignore_file(self):
+        if not os.path.isdir(self.directory):
+            _ = CommandLineException(f"{self.directory} is not a directory")
+            return None
+        files = list(filter(lambda current_element:current_element.endswith(".polyglot"), os.listdir(self.directory)))
+        print(files)
+
+def command_executor(results):
+    command, params = results.command, results.parameters
+    if command == "stats":
+        _ = LanguageStats(params)
+
 def main():
     arguments = sys.argv[1:]
     argument_parser = ArgumentParser(arguments)
     results = argument_parser.create_argument_parser()
-    print(results)
+    if results.command not in COMMANDS:
+        error = CommandLineException(f"Invalid command : {results.command}")
+    command_executor(results)
 
 if __name__ == "__main__":
     main()
